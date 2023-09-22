af1860988639ac3095751a7f5d26b2f6a15f24f2
AmiraTarek99 <105367615+AmiraTarek99@users.noreply.github.com>
2023-09-23 02:22:56 +0300

    Create installation.groovy

---
+++ vars/installation.groovy
@@ -0,0 +1,24 @@
+def call() {
+     pipline {
+       agent { label 'worker2' }
+       stages {
+        stage('Install Nginx') {
+          steps{
+            sh 'sudo apt update'
+            sh 'sudo apt install -y nginx'
+             } 
+           } 
+        stage('Start Nginx') {
+          steps{
+            sh 'sudo systemctl start nginx'
+             } 
+           } 
+        stage('Testing') {
+          steps{
+            sh 'curl localhost'
+             } 
+           } 
+
+        }
+    }
+}
