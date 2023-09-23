b56682cc4414758a41f8970f32cbd92e76aa3148
AmiraTarek99 <105367615+AmiraTarek99@users.noreply.github.com>
2023-09-23 04:52:44 +0300

    Update installation.groovy

--- vars/installation.groovy
+++ vars/installation.groovy
@@ -1,24 +1,23 @@
 def call() {
-     pipline {
-       agent { label 'worker2' }
-       stages {
-        stage('Install Nginx') {
-          steps{
-            sh 'sudo apt update'
-            sh 'sudo apt install -y nginx'
-             } 
-           } 
-        stage('Start Nginx') {
-          steps{
-            sh 'sudo systemctl start nginx'
-             } 
-           } 
-        stage('Testing') {
-          steps{
-            sh 'curl localhost'
-             } 
-           } 
-
+    pipeline {
+        agent { label 'worker2' }
+        stages {
+            stage('Install Nginx') {
+                steps {
+                    sh 'sudo apt update'
+                    sh 'sudo apt install -y nginx'
+                }
+            }
+            stage('Start Nginx') {
+                steps {
+                    sh 'sudo systemctl start nginx'
+                }
+            }
+            stage('Testing') {
+                steps {
+                    sh 'curl localhost'
+                }
+            }
         }
     }
 }
